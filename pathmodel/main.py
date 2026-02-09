0c8d99383206a47bfad83fe89d072dd121f7c9cf
PaulineGHG <octavia.phg@gmail.com>
2026-02-09 14:45:22 +0100

    start rework ReactionSiteExtraction.lp as TransformationsExtraction.lp for the more flexible method

--- pathmodel/main.py
+++ pathmodel/main.py
@@ -2,8 +2,8 @@ import pathmodel
 
 # pathmodel infer -i data.lp -o output_folder -s 100
 
-INPUT_FILE = '/home/phamongi/Documents/Packages/pathmodel/pathmodel/data/oxylipins_pwy.lp'
-OUTPUT_DIR = 'Outputs/oxylipins'
+INPUT_FILE = ''
+OUTPUT_DIR = '../Files/Outputs/oxylipins'
 
 pathmodel.pathmodel_analysis(INPUT_FILE, OUTPUT_DIR, step_limit=100)
 # pathmodel_plot -i OUT
