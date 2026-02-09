276bd871d2b2cee5c0b5ab50e5fed5d8c74e838c
PaulineGHG <octavia.phg@gmail.com>
2026-02-09 16:55:48 +0100

    start rework ReactionSiteExtraction.lp as TransformationsExtraction.lp for the more flexible method

--- pathmo2/main.py
+++ pathmo2/main.py
@@ -14,7 +14,7 @@ def generate_transformations(run_path):
 
     """
     print('~~~~~Creation of Reaction~~~~~')
-    reaction_site_extraction_script = os.path.join(*[ROOT, 'asp', 'ReactionSiteExtraction.lp'])
+    reaction_site_extraction_script = os.path.join(*[ROOT, 'asp', 'TransformationsExtraction.lp'])
     input_file = os.path.join(run_path, INPUT_DIR, LP_INPUT)
 
     reaction_solver = clyngor.solve([input_file, reaction_site_extraction_script], use_clingo_module=False)
@@ -24,51 +24,55 @@ def generate_transformations(run_path):
     transformation_products = {}
     reactions = []
     for atom in next(reaction_solver.parse_args.int_not_parsed.sorted):
-        reaction_results.append(atom[0] + '(' + ','.join(atom[1]) + ')')
-        if 'diff' in atom[0]:
-            reaction_id = atom[1][0]
-            substructures = atom[1][1:]
-            reactions.append(reaction_id)
-            if 'Before' in atom[0]:
-                if reaction_id not in transformation_reactants:
-                    transformation_reactants[reaction_id] = [substructures]
-                else:
-                    transformation_reactants[reaction_id].append([substructures])
-            elif 'After' in atom[0]:
-                if reaction_id not in transformation_products:
-                    transformation_products[reaction_id] = [substructures]
-                else:
-                    transformation_products[reaction_id].append([substructures])
-    print(transformation_reactants)
-    print(transformation_products)
-
-    reactions = set(reactions)
-    output_folder = os.path.join(run_path, OUTPUT_DIR)
-    pathmodel_output_transformation_path = os.path.join(output_folder, 'pathmodel_data_transformations.tsv')
-    with open(pathmodel_output_transformation_path, 'w') as transformation_file:
-        csvwriter = csv.writer(transformation_file, delimiter = '\t')
-        csvwriter.writerow(['reaction_id', 'reactant_sbustructure', 'product_substructure'])
-        for reaction in reactions:
-            if reaction in transformation_reactants:
-                reactant = transformation_reactants[reaction]
-            else:
-                reactant = []
-            if reaction in transformation_products:
-                product = transformation_products[reaction]
-            else:
-                product = []
-            csvwriter.writerow([reaction, reactant, product])
-
-
-    reaction_result = '\n'.join([atom+'.' for atom in reaction_results])
-
-    return reaction_result
+        print(atom)
+    #     reaction_results.append(atom[0] + '(' + ','.join(atom[1]) + ')')
+    #     if 'diff' in atom[0]:
+    #         reaction_id = atom[1][0]
+    #         substructures = atom[1][1:]
+    #         reactions.append(reaction_id)
+    #         if 'Before' in atom[0]:
+    #             if reaction_id not in transformation_reactants:
+    #                 transformation_reactants[reaction_id] = [substructures]
+    #             else:
+    #                 transformation_reactants[reaction_id].append([substructures])
+    #         elif 'After' in atom[0]:
+    #             if reaction_id not in transformation_products:
+    #                 transformation_products[reaction_id] = [substructures]
+    #             else:
+    #                 transformation_products[reaction_id].append([substructures])
+    # print(transformation_reactants)
+    # print(transformation_products)
+    #
+    # reactions = set(reactions)
+    # output_folder = os.path.join(run_path, OUTPUT_DIR)
+    # pathmodel_output_transformation_path = os.path.join(output_folder, 'pathmodel_data_transformations.tsv')
+    # with open(pathmodel_output_transformation_path, 'w') as transformation_file:
+    #     csvwriter = csv.writer(transformation_file, delimiter = '\t')
+    #     csvwriter.writerow(['reaction_id', 'reactant_sbustructure', 'product_substructure'])
+    #     for reaction in reactions:
+    #         if reaction in transformation_reactants:
+    #             reactant = transformation_reactants[reaction]
+    #         else:
+    #             reactant = []
+    #         if reaction in transformation_products:
+    #             product = transformation_products[reaction]
+    #         else:
+    #             product = []
+    #         csvwriter.writerow([reaction, reactant, product])
+    #
+    #
+    # reaction_result = '\n'.join([atom+'.' for atom in reaction_results])
+    #
+    # return reaction_result
 
 
 # ==================================================================================================
 
-RUN_PATH = '/home/phamongi/Documents/Dev/pathmodel/Files'
-RUN_NAME = 'Oxylipins'
+# RUN_PATH = '/home/phamongi/Documents/Dev/pathmodel/Files'
+RUN_PATH = 'C:\\Users\Octav\PycharmProjects\pathmodel\Files'
+RUN_NAME = 'ToyExemple'
 
-generate_lp_input(os.path.join(RUN_PATH, RUN_NAME), True)
+# generate_lp_input(os.path.join(RUN_PATH, RUN_NAME), True)
 generate_transformations(os.path.join(RUN_PATH, RUN_NAME))
+
+
