16e29d06df11d5c2f711ee35f52a02a5926ec71c
phamongi <octavia.phg@gmail.com>
2026-02-27 14:07:53 +0100

    extract transfo

--- pathmo2/main.py
+++ pathmo2/main.py
@@ -1,6 +1,8 @@
 import os
 import csv
 import clyngor
+import rdkit.Chem
+
 from pathmo2.input_management.parse_input import *
 
 
@@ -19,34 +21,34 @@ def generate_transformations(run_path):
 
     reaction_solver = clyngor.solve([input_file, transformations_script], use_clingo_module=False)
     result_atoms = []
-    transformation_reactants = {}
-    transformation_products = {}
-    reactions = []
+    transformations = {}
+    reactant_transformation_centers = {}
+    product_transformation_centers = {}
     for atom in next(reaction_solver.parse_args.int_not_parsed.sorted):
-        print(atom)
-        result_atoms.append(atom[0] + '(' + ', '.join(atom[1]) + ')')
-
+        atom_str = atom[0] + '(' + ', '.join(atom[1]) + ')'
+        result_atoms.append(atom_str)
+        # Extract transformations
+        if atom[0].endswith('Difference'):
+            if atom[1][0] not in transformations:
+                transformations[atom[1][0]] = []
+            transformations[atom[1][0]].append(atom)
+        # Extract Reactant Transformation Centers
+        if atom[0].startswith('reactantTransformationCenter'):
+            if atom[1][0] not in reactant_transformation_centers:
+                reactant_transformation_centers[atom[1][0]] = []
+            reactant_transformation_centers[atom[1][0]].append(atom)
+        # Extract Product Transformation Centers
+        if atom[0].startswith('productTransformationCenter'):
+            if atom[1][0] not in product_transformation_centers:
+                product_transformation_centers[atom[1][0]] = []
+            product_transformation_centers[atom[1][0]].append(atom)
 
     print(result_atoms)
+    print(transformations)
+    print(product_transformation_centers)
+    print(reactant_transformation_centers)
+    return result_atoms, transformations, product_transformation_centers, reactant_transformation_centers
 
-    #     if 'diff' in atom[0]:
-    #         reaction_id = atom[1][0]
-    #         substructures = atom[1][1:]
-    #         reactions.append(reaction_id)
-    #         if 'Before' in atom[0]:
-    #             if reaction_id not in transformation_reactants:
-    #                 transformation_reactants[reaction_id] = [substructures]
-    #             else:
-    #                 transformation_reactants[reaction_id].append([substructures])
-    #         elif 'After' in atom[0]:
-    #             if reaction_id not in transformation_products:
-    #                 transformation_products[reaction_id] = [substructures]
-    #             else:
-    #                 transformation_products[reaction_id].append([substructures])
-    # print(transformation_reactants)
-    # print(transformation_products)
-    #
-    # reactions = set(reactions)
     # output_folder = os.path.join(run_path, OUTPUT_DIR)
     # pathmodel_output_transformation_path = os.path.join(output_folder, 'pathmodel_data_transformations.tsv')
     # with open(pathmodel_output_transformation_path, 'w') as transformation_file:
@@ -67,6 +69,10 @@ def generate_transformations(run_path):
     # reaction_result = '\n'.join([atom+'.' for atom in reaction_results])
     #
     # return reaction_result
+
+
+def export_transformations_patterns():
+    product_transformation_pattern = rdkit.Chem.Mol
 
 
 # ==================================================================================================
